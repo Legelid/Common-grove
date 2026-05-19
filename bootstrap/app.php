61ab728cdcfc7f1682efbfed5ae5c26b9b5a88f0
Andrew <andrewrcollins@live.com>
2026-05-19 09:37:20 -0400

    CommmonGrove Beta - V0.1 - Editing URL refresh bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,7 +22,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         // Trust all upstream proxy headers so Laravel sees the real client
         // IP, protocol (HTTPS), and host when behind Nginx or any load balancer.
-        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL);
+        $middleware->trustProxies(at: '*', headers:
+            \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR
+            | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST
+            | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT
+            | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO
+        );
 
         $middleware->append(SecurityHeaders::class);
         $middleware->web(append: [UpdateLastSeen::class, CheckSuspended::class, EnsureDateOfBirth::class]);
