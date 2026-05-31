9c29feed1d7451a9524c860ac6e1642aa197a473
Andrew <andrewrcollins@live.com>
2026-05-31 19:04:41 -0400

    CommmonGrove Beta - V0.2 - Fixing password sign in issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+// https://vitejs.dev/config/
 export default defineConfig({
     server: {
         host: 'localhost',
